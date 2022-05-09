7704ec1b9a752169ef1a9dcda035ca06a27ada22
DaphneLi <daphnel.cisnerosmartinez@gmail.com>
2022-05-09 12:29:26 -0500

    conexion nodo

---
+++ config/db.py
@@ -0,0 +1,6 @@
+from pymongo import MongoClient
+
+mongo_Daphne= "mongodb://mongo:9uqlMcN0A2sto2QdgWc5@containers-us-west-52.railway.app:6888"
+MySQL_Daphne="-hcontainers-us-west-54.railway.app -uroot -pmE73tnaYDSaTByOIJHZh --port 6848 --protocol=TCP railway"
+conn= MongoClient(mongo_Daphne)
+
