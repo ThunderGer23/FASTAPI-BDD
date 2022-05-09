8fb62d1dad1a4ed58e8a8b4f6f5353484cde8cf3
ThunderGer <Jaames11@hotmail.com>
2022-05-09 12:33:20 -0500

    conexión a BD Mongo y PostgreSQL

---
+++ config/db.py
@@ -0,0 +1,6 @@
+from pymongo import MongoClient
+
+mongo_ThunderGer = 'mongodb://mongo:8A58CKJLY90VZ54mXAxt@containers-us-west-52.railway.app:6953'
+PostgreSQL_ThunderGer = 'postgresql://postgres:1AhTI248pPGdta2fLGuu@containers-us-west-52.railway.app:7012/railway'
+
+conn = MongoClient(mongo_url)
