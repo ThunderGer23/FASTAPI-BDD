99a1aadbef20f677888b2976f18ad851faf555dd
ThunderGer <Jaames11@hotmail.com>
2022-05-11 12:14:41 -0500

    add routes for github

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from routes.hero import hero
+from routes.github import github
 
 app = FastAPI()
 
 app.include_router(hero)
+app.include_router(github)
---
+++ routes/github.py
@@ -0,0 +1,62 @@
+from fastapi import APIRouter, UploadFile, File
+from fastapi.responses import JSONResponse
+from os import getcwd, remove, mkdir
+from shutil import rmtree
+import json
+import csv
+
+github = APIRouter()
+
+@github.post("/uploadfile/github")
+async def upload_file(file: UploadFile = File(...)):
+    csvFilePath = getcwd() + '/docs/github/' + file.filename
+    jsonFilePath = f'{csvFilePath.replace(".csv","")}.json'
+    data = []
+    
+    try:
+        mkdir(getcwd() + '/docs/github/')
+    except FileExistsError:
+        pass
+
+    with open(csvFilePath, 'wb') as myfile:
+        content = await file.read()
+        myfile.write(content)
+        myfile.close()
+
+    with open(csvFilePath, encoding='utf-8') as csvf:
+        csvReader = csv.DictReader(csvf)
+        for rows in csvReader:
+            data.append(rows)
+
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
+        jsonf.write(json.dumps(data, indent=4))
+    
+    return "Success!"
+    
+@github.delete("/delete/file_github/{name_file}")
+def delete_file(name_file: str):
+    try:
+        remove(getcwd() + '/docs/github/' + name_file)
+        return JSONResponse(content = {
+            "removed": True,
+            "message": "File Remove Successfully"
+        }, status_code = 200)
+    except FileNotFoundError:
+        return JSONResponse(content = {
+            "removed": False,
+            "message": "File not found"
+        }, status_code = 404)
+
+@github.delete("/folder/github/{folder_name}")
+def delete_folder(folder_name: str):
+    try:
+        rmtree(getcwd() + '/docs/' + folder_name)
+        return JSONResponse(content = {
+            "removed": True,
+            "message": "File Remove Successfully"
+        }, status_code = 200)
+    except FileNotFoundError:
+        return JSONResponse(content = {
+            "removed": False,
+            "message": "Folder not found"
+        }, status_code = 404)
