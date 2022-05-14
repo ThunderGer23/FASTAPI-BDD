a10212c6c44fad9cfa9b7010fb3b2aa08e220cb1
ThunderGer <Jaames11@hotmail.com>
2022-05-13 21:11:30 -0500

    db

--- config/db.py
+++ config/db.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 
 mongo_ThunderGer = 'mongodb://mongo:8A58CKJLY90VZ54mXAxt@containers-us-west-52.railway.app:6953'
+
 PostgreSQL_ThunderGer = 'postgresql://postgres:1AhTI248pPGdta2fLGuu@containers-us-west-52.railway.app:7012/railway'
 
 conn = MongoClient(mongo_ThunderGer)
