512f82dfc763b3ab57eb508a98b9412422763ffe
ThunderGer <Jaames11@hotmail.com>
2022-05-13 21:17:03 -0500

    Agregando DB de Melissa

--- config/db.py
+++ config/db.py
@@ -8,3 +8,6 @@ mongo_ThunderGer = 'mongodb://mongo:8A58CKJLY90VZ54mXAxt@containers-us-west-52.r
 PostgreSQL_ThunderGer = 'postgresql://postgres:1AhTI248pPGdta2fLGuu@containers-us-west-52.railway.app:7012/railway'
 
 conn2 = MongoClient(mongo_url)
+
+mongo_Melissa = 'mongodb://mongo:Dk9kXqiOWXEpOgPvwDMm@containers-us-west-32.railway.app:5627'
+MySQL_Melissa = 'mysql://root:48NJIAl2aPaASmGPqNsK@containers-us-west-53.railway.app:6445/railway'
