80e1075b2de7cf5f418635ea67135c3dab8d6d30
ThunderGer <Jaames11@hotmail.com>
2022-05-13 21:34:20 -0500

    Refactor db.py

--- config/db.py
+++ config/db.py
@@ -1,18 +1,24 @@
+from sqlite3.dbapi2 import _Parameters
 from pymongo import MongoClient
 
-mongo_Daphne= "mongodb://mongo:9uqlMcN0A2sto2QdgWc5@containers-us-west-52.railway.app:6888"
-MySQL_Daphne="-hcontainers-us-west-54.railway.app -uroot -pmE73tnaYDSaTByOIJHZh --port 6848 --protocol=TCP railway"
-conn1= MongoClient(mongo_Daphne)
+# ? Todas las claves de acceso que tiene este archivo deben eliminarse y accederse desde variables de entorno dedicadas para el repositorio
 
-mongo_ThunderGer = 'mongodb://mongo:8A58CKJLY90VZ54mXAxt@containers-us-west-52.railway.app:6953'
+MonClient = [{
+    'ThunderGer': 'mongodb://mongo:8A58CKJLY90VZ54mXAxt@containers-us-west-52.railway.app:6953',
+    'Daphne' : 'mongodb://mongo:9uqlMcN0A2sto2QdgWc5@containers-us-west-52.railway.app:6888',
+    'Melissa' : 'mongodb://mongo:Dk9kXqiOWXEpOgPvwDMm@containers-us-west-32.railway.app:5627'
 
-PostgreSQL_ThunderGer = 'postgresql://postgres:1AhTI248pPGdta2fLGuu@containers-us-west-52.railway.app:7012/railway'
+}]   
 
-<<<<<<< HEAD
-conn2 = MongoClient(mongo_url)
+SQLClient = [{
+    'ThunderGer': 'postgresql://postgres:1AhTI248pPGdta2fLGuu@containers-us-west-52.railway.app:7012/railway',
+    'Daphne' : '', #TODO: Add link por connection in this area
+    'Melissa' : 'mysql://root:48NJIAl2aPaASmGPqNsK@containers-us-west-53.railway.app:6445/railway'
+}]
 
-mongo_Melissa = 'mongodb://mongo:Dk9kXqiOWXEpOgPvwDMm@containers-us-west-32.railway.app:5627'
-MySQL_Melissa = 'mysql://root:48NJIAl2aPaASmGPqNsK@containers-us-west-53.railway.app:6445/railway'
-=======
-conn = MongoClient(mongo_ThunderGer)
->>>>>>> ThunderGer
+
+#TODO: Check link for connection
+#MySQL_Daphne="-hcontainers-us-west-54.railway.app -uroot -pmE73tnaYDSaTByOIJHZh --port 6848 --protocol=TCP railway"
+
+Mon = MongoClient(MonClient)
+#MySQL = MongoClient(mongo_url) -> this is for MySQL
