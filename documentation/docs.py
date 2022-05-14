359ff6174593b8d4fb5a67d06dd57bc297e6dec9
ThunderGer <Jaames11@hotmail.com>
2022-05-13 19:26:50 -0500

    Modificando rutas y agregando documentación

--- documentation/docs.py
+++ documentation/docs.py
@@ -1,9 +1,19 @@
 tags_metadata = [{
-    "name": "Github",
+    "name": 'Github',
     "description": "Rutas para obtener información de repositorios en GitHub"
 }, {
-    "name": "Hero",
+    "name": 'Hero',
     "description": "Rutas para obtener información de los superheroes"
 }, {
-    "name": "Medium"
+    "name": "Medium",
+    "description": "Rutas para obtener información para medium"
+}, {
+    "name": 'npm',
+    "description": "Rutas para obtener información de npm"
+}, {
+    "name": 'Programming',
+    "description": "Rutas para obtener información de los distintos lenguajes de programación"
+}, {
+    "name": 'Steam'
+    "description": "Rutas para obtener información de los VideoJuegos"
 }]
